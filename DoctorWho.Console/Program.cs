9fa2fca90063e15afb3b7aff716598176615d1fb
Asma Abu Saa <asmaabusaa4@gmail.com>
2023-02-04 22:18:34 +0200

    Initial Structure

---
+++ DoctorWho.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DoctorWho.Db;
+using DoctorWho.Db.DataModels;
+
+using (DoctorWhoCoreDbContext doctorWhoCoreDbContext = new DoctorWhoCoreDbContext())
+{
+    doctorWhoCoreDbContext.Database.EnsureCreated();
+}
+
+
+////GetDoctors();
+
+//void GetDoctors()
+//{
+//    using var context = new DoctorWhoCoreDbContext();
+//    var episods = context.Doctors.ToList();
+//    foreach (var e in episods)
+//        Console.WriteLine(e.DoctorName);
+//    var state = context.ChangeTracker.DebugView.ShortView;
+//}
+
+////AddDoctors();
+//void AddDoctors()
+//{
+//    using var context=new DoctorWhoCoreDbContext();
+//     context.Doctors.AddRange(
+// new Doctor {  DoctorNumber = 1, DoctorName = "William Hartnell", BirthDate = DateTime.Now.Date, FirstEpisodeDate = DateTime.Now.Date, LastEpisodeDate = DateTime.Now.Date },
+// new Doctor { DoctorNumber = 2, DoctorName = "Peter Capaldi", BirthDate = DateTime.Now.Date, FirstEpisodeDate = DateTime.Now.Date, LastEpisodeDate = DateTime.Now.Date },
+// new Doctor { DoctorNumber = 3, DoctorName = "Jon Pertwee", BirthDate = DateTime.Now.Date, FirstEpisodeDate = DateTime.Now.Date, LastEpisodeDate = DateTime.Now.Date },
+// new Doctor { DoctorNumber = 4, DoctorName = "Patrick Troughton", BirthDate = DateTime.Now.Date, FirstEpisodeDate = DateTime.Now.Date, LastEpisodeDate = DateTime.Now.Date },
+// new Doctor {  DoctorNumber = 5, DoctorName = "Sylvester McCoy", BirthDate = DateTime.Now.Date, FirstEpisodeDate = DateTime.Now.Date, LastEpisodeDate = DateTime.Now.Date }
+// );
+//    context.SaveChanges();
+//}
---
+++ DoctorWho.Db/DoctorWhoCoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DoctorWho.Db.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorWho.Db;
+public class DoctorWhoCoreDbContext : DbContext
+{
+    public DbSet<Episode> Episodes { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Doctor> Doctors { get; set; }
+    public DbSet<Companion> Companions { get; set; }
+    public DbSet<Enemy> Enemies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+          "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = DoctorWhoCore"
+        );
+    }
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+    //    base.OnModelCreating(modelBuilder);
+    //}
+
+}
